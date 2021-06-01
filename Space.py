3f5644b543bdcad797c61915f23fbc634db327c0
SiddharthSinghTanwar <83647524+SiddharthSinghTanwar@users.noreply.github.com>
2021-06-01 14:53:52 +0530

    Uploaded after making in jupyter online
    Finding the ISS

---
+++ Space.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import  pandas as pd
+import plotly.express as px
+
+
+# In[6]:
+
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+
+# In[7]:
+
+
+df = pd.read_json(url)
+
+
+# In[8]:
+
+
+df
+
+
+# In[9]:
+
+
+df['latitude'] = df.loc['latitude','iss_position']
+df['longitude'] = df.loc['longitude','iss_position']
+df.reset_index(inplace=True)
+
+
+# In[10]:
+
+
+df
+
+
+# In[11]:
+
+
+df = df.drop(['index','message'], axis=1)
+
+
+# In[12]:
+
+
+df
+
+
+# In[13]:
+
+
+fig = px.scatter_geo(df, lat='latitude',
+                    lon= 'longitude')
+
+fig.show()
+
+
+# In[ ]:
+
+
+
+
