f58fbd7b0073981a10695cd7907695d3acbf66dc
Siddharth Singh Tanwar <siddharthtanwar28@gmail.com>
2021-12-14 20:06:47 +0530

    A typing speed checker game
    This game runs on command line, and uses the module curses which needs to be installed separately using pip.
    credits: Tech with Tim

---
+++ tutorial.py
@@ -0,0 +1,56 @@
+import curses
+from curses import wrapper
+import time
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Welcome to the Speed Typing Test!')
+    stdscr.addstr('\nPress any key to begin!')
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr, target, current , wpm = 0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addscr(0, i, char,  color)
+
+def wpm_test(stdscr):
+    target_text = "hello world this is some test text for this application!"
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+
+    while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        stdscr.clear()
+       
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        if key in ('KEY_BACKSPACE', '\b', '\x7f'):
+            if len(current_text) > 0 :
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+
+        
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    
+    start_screen(stdscr)
+    wpm_test(stdscr)
+    
+
+wrapper(main)
